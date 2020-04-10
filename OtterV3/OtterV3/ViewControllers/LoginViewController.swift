1f61bbd8a103b35afcd8f38ae7115346e139a97f
Malik <arachich@msu.edu>
2020-04-10 19:25:28 -0400

    Initial commit

---
+++ OtterV3/OtterV3/ViewControllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  OtterV3
+//
+//  Created by Malik Arachiche on 4/10/20.
+//  Copyright © 2020 Malik Arachiche. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+
+}
